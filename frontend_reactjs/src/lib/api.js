63f001bd10d0165ec3afed77596fc864daaad495
Your Name <you@example.com>
2025-11-12 21:22:43 +0000

    compose

--- frontend_reactjs/src/lib/api.js
+++ frontend_reactjs/src/lib/api.js
@@ -1,5 +1,5 @@
 // API base URL - Gateway service
-const API_BASE_URL = 'http://gateway-service:8080/api';
+const API_BASE_URL = 'http://gateway-service:8081/api/';
 
 // Get auth token from localStorage
 function getAuthToken() {
